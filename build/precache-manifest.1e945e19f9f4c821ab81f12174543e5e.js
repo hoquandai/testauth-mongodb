980a01d20f5c5e0185ff7132013d58b06c655d35
Dai Ho <daiho@teqnological.asia>
2020-01-03 16:59:24 +0700

    upload

---
+++ build/precache-manifest.1e945e19f9f4c821ab81f12174543e5e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c47b19d36091b735df86890671dcdb8",
+    "url": "/testauth/index.html"
+  },
+  {
+    "revision": "692bd1388b4c007a7a29",
+    "url": "/testauth/static/js/2.6a10a18f.chunk.js"
+  },
+  {
+    "revision": "6c7f5d68ecb208e8f5e7bc0bd4d8164a",
+    "url": "/testauth/static/js/2.6a10a18f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "853871d5ada75e797ad4",
+    "url": "/testauth/static/js/main.6a0ad7fd.chunk.js"
+  },
+  {
+    "revision": "4a1864e6f6a81e3571c3",
+    "url": "/testauth/static/js/runtime-main.c0276af6.js"
+  }
+]);
